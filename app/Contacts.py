191d4a376c0cc76633f9b431b79e6d9e95dbe567
Vivek Sharma <vivshrmav4@gmail.com>
2021-11-14 14:09:05 +0530

    Initial commit

---
+++ app/Contacts.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry, END, StringVar
+from tkinter import ttk, Scrollbar, Toplevel
+
+class contacts:
+    def __init__(self):
+        self.root = root
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("My Contact List")
+    root.mainloop()
